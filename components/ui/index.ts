82e30b462692acdf47aa54d3f226cfbfcd3c6cf6
Ivan Khurbatau <hurbatushka@gmail.com>
2024-06-27 06:50:23 +0300

    initial project, header and conatiner as shared components and some ui components.

---
+++ components/ui/index.ts
@@ -0,0 +1,14 @@
+export { Button } from "./button";
+export { Badge } from "./badge";
+export { Checkbox } from "./checkbox";
+export { Dialog } from "./dialog";
+export { Drawer } from "./drawer";
+export { Input } from "./input";
+export { Popover } from "./popover";
+export { RangeSlider } from "./range-slider";
+export { Select } from "./select";
+export { Sheet } from "./sheet";
+export { Skeleton } from "./skeleton";
+export { Slider } from "./slider";
+export { Table } from "./table";
+export { Textarea } from "./textarea";
